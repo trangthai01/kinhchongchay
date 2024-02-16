fce1525739b5ec145c2799505bf258db92f56f6e
I100000001-W01A\trang-pc <trangntt@group.yanolja.com>
2024-02-16 19:33:52 +0700

    next

--- resources/views/layouts/pages/engineering/applications/pivot-door-system.blade.php
+++ resources/views/layouts/pages/engineering/applications/pivot-door-system.blade.php
@@ -9,11 +9,11 @@
     <div class="system-main">
         <div class="container-fluid no-ltr-padding">
             <div class="system-main__list parallex-800 col-sm-12 no-ltr-padding">
-                <img src="https://tecfires.com/wp-content/uploads/2017/05/MAIN-PIC-PIVOT-LOW-1920x800.jpg" srcset="https://tecfires.com/wp-content/uploads/2017/05/MAIN-PIC-PIVOT-LOW.jpg 1x" class="img-responsive" alt="">			<div class="system-main__mask valign-bottom" style="display: none;">
+                <img src="{{ asset('images/pages/MAIN-PIC-PIVOT-LOW.jpg')}}" class="img-responsive" alt="">			<div class="system-main__mask valign-bottom" style="display: none;">
                     <div class="text-holder">
                         <h1><strong>PIVOT DOOR</strong> SYSTEM</h1>
                         <p>A new concept which eliminates any visual impact of components such as hinges or springs, tested and certified in accordance with the different international regulations.</p>
-    <p>The design of this system fulfils the most stringent performance and technical requirements with regard to glass protection, providing the largest tested and certified dimensions for doors of these characteristics.</p>
+    					<p>The design of this system fulfils the most stringent performance and technical requirements with regard to glass protection, providing the largest tested and certified dimensions for doors of these characteristics.</p>
                     </div>
                 </div>
             </div>
@@ -50,151 +50,116 @@
                 </div>	
             </div>
         </div>
-<section class="section section-cta">
-	<div class="container">
-		<div class="row company-top padding-bottom">
-			<div class="col-sm-4 col-md-3">
-				<div class="info-title color-primary">
-					<h2>Pivot Door System</h2>
+		<section class="section section-cta">
+			<div class="container">
+				<div class="row company-top padding-bottom">
+					<div class="col-sm-4 col-md-3">
+						<div class="info-title color-primary">
+							<h2>Pivot Door System</h2>
+						</div>
+					</div>
+					<div class="col-sm-8 col-md-9 text-holder">
+						<p>A new concept which eliminates any visual impact of components such as hinges or springs, tested and certified in accordance with the different international regulations.</p>
+						<p>The design of this system fulfils the most stringent performance and technical requirements with regard to glass protection, providing the largest tested and certified dimensions for doors of these characteristics.</p>
+					</div>
 				</div>
 			</div>
-			<div class="col-sm-8 col-md-9 text-holder">
-				<p>A new concept which eliminates any visual impact of components such as hinges or springs, tested and certified in accordance with the different international regulations.</p>
-<p>The design of this system fulfils the most stringent performance and technical requirements with regard to glass protection, providing the largest tested and certified dimensions for doors of these characteristics.</p>
-			</div>
-		</div>
-	</div>
-</section>
-<div class="section section-detail no-space" id="details-top">
-	<div class="container">
+		</section>
+		<div class="section section-detail no-space" id="details-top">
+			<div class="container">
 				<div class="row">
-
-				<div class="col-sm-5 door-gallery door-gallery-height">
-		<div class="sliding-door-holder border-all">
-			<div class="slidingDoor-carousel all-gallery">
+					<div class="col-sm-5 door-gallery door-gallery-height">
+						<div class="sliding-door-holder border-all">
+							<div class="slidingDoor-carousel all-gallery">
 								<div class="carousel-cell">
-					<img src="https://tecfires.com/wp-content/uploads/2017/05/SP-PD-1-Copy.jpg" class="img-responsive" alt="">					<div class="text-holder" style="display: none;">Fire Rated Steel &amp; Stainless Steel<br>
-Pivot Door System</div>				</div>
+									<img src="{{ asset('images/pages/SP-PD-1-Copy.jpg')}}" class="img-responsive" alt="">					
+									<div class="text-holder" style="display: none;">Fire Rated Steel &amp; Stainless Steel<br>Pivot Door System</div>				
+								</div>
 								<div class="carousel-cell">
-					<img src="https://tecfires.com/wp-content/uploads/2017/05/pivot2.png" class="img-responsive" alt="">					<div class="text-holder" style="display: none;">Burglar Proof Steel &amp; Stainless Steel<br>
-Pivot Door System</div>				</div>
+									<img src="{{ asset('images/pages/pivot2.png')}}" class="img-responsive" alt="">					
+									<div class="text-holder" style="display: none;">Burglar Proof Steel &amp; Stainless Steel<br>Pivot Door System</div>				
+								</div>
 								<div class="carousel-cell">
-					<img src="https://tecfires.com/wp-content/uploads/2017/05/PIVOT-RENDER-3-LOW.jpg" class="img-responsive" alt="">					<div class="text-holder" style="display: none;">Full Size<br>
-Pivot Door System<br>
-</div>				</div>
+									<img src="{{ asset('images/pages/PIVOT-RENDER-3-LOW.jpg')}}" class="img-responsive" alt="">					
+									<div class="text-holder" style="display: none;">Full Size<br>Pivot Door System<br></div>				
+								</div>
 								<div class="carousel-cell">
-					<img src="https://tecfires.com/wp-content/uploads/2017/05/pivot4.jpg" class="img-responsive" alt="">					<div class="text-holder" style="display: none;">Pivot Door System<br>
-Detail<br>
-</div>				</div>
+									<img src="{{ asset('images/pages/pivot4.jpg')}}" class="img-responsive" alt="">					
+									<div class="text-holder" style="display: none;">Pivot Door System<br>Detail<br>
+									</div>				
+								</div>
 								<div class="carousel-cell">
-					<img src="https://tecfires.com/wp-content/uploads/2017/05/pivot5.jpg" class="img-responsive" alt="">					<div class="text-holder" style="display: none;">Pivot Door System<br>
-Detail<br>
-</div>				</div>
+									<img src="{{ asset('images/pages/pivot5.jpg')}}" class="img-responsive" alt="">					
+									<div class="text-holder" style="display: none;">Pivot Door System<br>Detail<br></div>				
+								</div>
 							</div>
-			<div id="cell-cap"></div>
-		</div>
-	</div>
-
-	
-			<div class="col-sm-7 tabs-holder door-gallery-height">
-				<div role="tabpanel">
-					<ul class="nav nav-tabs" role="tablist">
-												    <li role="presentation" class="active">
-						        <a href="#description" aria-controls="description" role="tab" data-toggle="tab">Description</a>
-						    </li>
-												    <li role="presentation">
-						        <a href="#technical" aria-controls="technical" role="tab" data-toggle="tab">Technical data</a>
-						    </li>
-												    <li role="presentation">
-						        <a href="#brochures" aria-controls="brochures" role="tab" data-toggle="tab">Brochures</a>
-						    </li>
+							<div id="cell-cap"></div>
+						</div>
+					</div>
+					<div class="col-sm-7 tabs-holder door-gallery-height">
+						<div role="tabpanel">
+							<ul class="nav nav-tabs" role="tablist">
+								<li role="presentation" class="active">
+									<a href="#description" aria-controls="description" role="tab" data-toggle="tab">Description</a>
+								</li>
+								<li role="presentation">
+									<a href="#technical" aria-controls="technical" role="tab" data-toggle="tab">Technical data</a>
+								</li>
+								<li role="presentation">
+									<a href="#brochures" aria-controls="brochures" role="tab" data-toggle="tab">Brochures</a>
+								</li>
+							</ul>
+							<div class="tab-content">
+								<div role="tabpanel" class="tab-pane active" id="description">
+									<div class="tabs-text">
+										<p>PIVOT DOOR SYSTEM is a Special-CMS (Custom-Made-Solution) available as fire rated or burglar proof glass solution, based on profiles made of steel or stainless steel with thermal breakage, and different options of automatic opening and closing mechanisms.</p>
+										<p>The PIVOT DOOR SYSTEM solution guarantees a high level of performance in any climatic condition.</p>
+										<div class="col-md-12 desc-list-holder">
+											<ul class="desc-list">
+												<li>Range of solutions: Galvanized steel (S235JRG2 EN10025) and stainless steel (304 &amp; 316) profiles.</li>
+												<li>High thermal and acoustic insulation.</li>
+												<li>Internal &amp; external applications.</li>
+												<li>Great aesthetic finish.</li>
+												<li>Tested in accordance with European (EN) and American (NFPA) standards.</li>
+												<li>Great structural stability, which makes it possible to maximize the dimensions of the glazed area.</li>
+												<li>Different options of automatic opening and closing mechanisms.</li>
 											</ul>
-
-					<div class="tab-content">
-					    <div role="tabpanel" class="tab-pane active" id="description">
-	<div class="tabs-text">
-
-		<p>PIVOT DOOR SYSTEM is a Special-CMS (Custom-Made-Solution) available as fire rated or burglar proof glass solution, based on profiles made of steel or stainless steel with thermal breakage, and different options of automatic opening and closing mechanisms.</p>
-<p>The PIVOT DOOR SYSTEM solution guarantees a high level of performance in any climatic condition.</p>
-<div class="col-md-12 desc-list-holder"><ul class="desc-list">
-<li>Range of solutions: Galvanized steel (S235JRG2 EN10025) and stainless steel (304 &amp; 316) profiles.</li>
-<li>High thermal and acoustic insulation.</li>
-<li>Internal &amp; external applications.</li>
-<li>Great aesthetic finish.</li>
-<li>Tested in accordance with European (EN) and American (NFPA) standards.</li>
-<li>Great structural stability, which makes it possible to maximize the dimensions of the glazed area.</li>
-<li>Different options of automatic opening and closing mechanisms.</li>
-</ul>
-</div>
-		<div class="col-sm-12 no-ltr-padding">
-    <ul class="desc-icon-list has-4">
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/1-Fire-Rated-3-150x150.jpg" alt=""></div><div class="text-holder">Fire Rated</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/4-EI60-90-EI-Integrity-Insulation-1-150x150.jpg" alt=""></div><div class="text-holder">EI (Integrity &amp; Insulation)</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/2-Steel-Stainless-Steel-Framing-3-150x150.jpg" alt=""></div><div class="text-holder">Steel &amp; Stainless Steel Framing</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/6-European-Standard-3-150x150.jpg" alt=""></div><div class="text-holder">European Standard</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/1-Burglar-Proof-Rated-150x150.jpg" alt=""></div><div class="text-holder">Burglar Proof Rated</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/4-RC2-RC3-Tecguard-RC-Classification-1-150x150.jpg" alt=""></div><div class="text-holder">Tecguard RC Classification</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/3-Internal-External-Application-3-150x150.jpg" alt=""></div><div class="text-holder">Internal &amp; External Application</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/7-American-Standard-1-150x150.jpg" alt=""></div><div class="text-holder">American Standard</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/5-Automatic-Systems-1-150x150.jpg" alt=""></div><div class="text-holder">Automatic Systems</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/10-Emergency-Exit-3-150x150.jpg" alt=""></div><div class="text-holder">Emergency Exit</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/9-Acoustic-Reduction-3-150x150.jpg" alt=""></div><div class="text-holder">Acoustic Reduction</div>            </li>
-                    <li>
-                <div class="image-holder"><img src="https://tecfires.com/wp-content/uploads/2017/05/8-Hose-Stream-Testing-2-150x150.jpg" alt=""></div><div class="text-holder">Hose Stream Testing</div>            </li>
-            </ul>
-</div>
-	</div>
-</div>
-<div role="tabpanel" class="tab-pane" id="technical">
-    <div class="tabs-text">
-        <div class="col-sm-12 no-ltr-padding">
-            <ul class="pdf-icon-list">
-                            </ul>
-        </div>
-    </div>
-</div>
-<div role="tabpanel" class="tab-pane" id="brochures">
-    <div class="tabs-text">
-        <div class="col-sm-12 no-ltr-padding">
-            <ul class="pdf-icon-list">
-                                <li>
-                    <a href="#" target="_blank" title="">
-
-                        <div class="link-holder">TECFIRE CATALOGUE</div>
-                    </a>
-                </li>
-                            </ul>
-        </div>
-    </div>
-</div>
+										</div>
+										<div class="col-sm-12 no-ltr-padding">
+    										<ul class="desc-icon-list has-4">
+												<li><div class="image-holder"><img src="{{ asset('images/pages/1-Fire-Rated-3-150x150.jpg')}}" alt=""></div><div class="text-holder">Fire Rated</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/4-EI60-90-EI-Integrity-Insulation-1-150x150.jpg')}}" alt=""></div><div class="text-holder">EI (Integrity &amp; Insulation)</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/2-Steel-Stainless-Steel-Framing-3-150x150.jpg')}}" alt=""></div><div class="text-holder">Steel &amp; Stainless Steel Framing</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/6-European-Standard-3-150x150.jpg')}}" alt=""></div><div class="text-holder">European Standard</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/1-Burglar-Proof-Rated-150x150.jpg')}}" alt=""></div><div class="text-holder">Burglar Proof Rated</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/4-RC2-RC3-Tecguard-RC-Classification-1-150x150.jpg')}}" alt=""></div><div class="text-holder">Tecguard RC Classification</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/3-Internal-External-Application-3-150x150.jpg')}}" alt=""></div><div class="text-holder">Internal &amp; External Application</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/7-American-Standard-1-150x150.jpg')}}" alt=""></div><div class="text-holder">American Standard</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/5-Automatic-Systems-1-150x150.jpg')}}" alt=""></div><div class="text-holder">Automatic Systems</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/10-Emergency-Exit-3-150x150.jpg')}}" alt=""></div><div class="text-holder">Emergency Exit</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/9-Acoustic-Reduction-3-150x150.jpg')}}" alt=""></div><div class="text-holder">Acoustic Reduction</div>            </li>
+												<li><div class="image-holder"><img src="{{ asset('images/pages/8-Hose-Stream-Testing-2-150x150.jpg')}}" alt=""></div><div class="text-holder">Hose Stream Testing</div>            </li>
+            								</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
-<div class="section section-gallery section-gallery-secondary">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="bg-title">
-					<h3>Image gallery</h3>
-				</div>
-				<div class="system-gallery">
-					<div class="col-sm-7">
-						<figure class="image-holder border-all">
-															<a class="system-gallery-s system-gallery-show-0 hide-system active" data-toggle="modal" data-type="p" data-index="0" data-show="0" href="#modal-engineering-projects">
+		<div class="section section-gallery section-gallery-secondary">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="bg-title">
+							<h3>Image gallery</h3>
+						</div>
+							<div class="system-gallery">
+								<div class="col-sm-7">
+									<figure class="image-holder border-all">
+										<a class="system-gallery-s system-gallery-show-0 hide-system active" data-toggle="modal" data-type="p" data-index="0" data-show="0" href="#modal-engineering-projects">
 									<img src="https://tecfires.com/wp-content/uploads/2017/05/AP-GPD-pop-up-01-1210x1170.jpg" class="img-responsive" alt=""><figcaption>Louvre Museum<br>Abu Dhabi  (United arab Emirates)</figcaption>								</a>
 															<a class="system-gallery-s system-gallery-show-1 hide-system" data-toggle="modal" data-type="p" data-index="1" data-show="1" href="#modal-engineering-projects">
 									<img src="https://tecfires.com/wp-content/uploads/2017/05/AP-GPD-pop-up-02-1210x1170.jpg" class="img-responsive" alt=""><figcaption>Doha Oasis<br>Doha (Qatar)</figcaption>								</a>
